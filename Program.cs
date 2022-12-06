53c46bfe3c516ec4aefba4eca640f5a578cacd54
Ksy-e <ksy-e@ya.ru>
2022-12-07 01:06:55 +0500

    Добавлено решение задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] FindValueLess(string[] array, int n)
+{
+    string resultString = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= n) resultString = resultString + array[i] + " ";
+    }
+    string[] newArray = resultString.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    return newArray;
+}
+
+Console.Write("Введите элементы массива через пробел: ");
+string input = (Console.ReadLine()!);
+int n = 3;
+string[] array = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", FindValueLess(array, n))}]");
